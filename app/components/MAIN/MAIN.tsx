ed398fd6a809a66fc27df089efec5c5adb40974b
Rei2718 <once2718@gmail.com>
2024-02-08 17:28:56 +0900

    001

--- app/components/MAIN/MAIN.tsx
+++ app/components/MAIN/MAIN.tsx
@@ -98,7 +98,7 @@ export default function MAIN() {
           </div>
         </nav>
 
-        <div className="bg-fixed h-screen flex justify-center" style={{ backgroundImage: 'url("bg_water.jpg")', backgroundSize: 'cover', backgroundPosition: 'center', height: '100%' }}>
+        <div className="bg-fixed h-screen flex justify-center" style={{ backgroundImage: 'url("bg_water.jpg")', backgroundSize: 'cover', backgroundPosition: 'center', height: '100lvh' }}>
           
           <div className={activeTab === '001' ? 'block' : 'hidden'} id="001">
             PAGE1
