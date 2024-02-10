629f3f35e550ef736034e8b39abfe89940d4b4bd
Rei2718 <once2718@gmail.com>
2024-02-11 00:28:07 +0900

    001

--- app/components/navigation1.tsx
+++ app/components/navigation1.tsx
@@ -60,7 +60,6 @@ const Navigation1 = ({
                       </div>
                     </Link>
                   </div>
-                
                 </div>
               </div>
             </div>
